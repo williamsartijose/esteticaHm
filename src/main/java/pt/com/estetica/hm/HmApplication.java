c662e729e70266c0ee13c4dba0887945542e91f5
william sarti <williamsartijose@hotmail.com>
2025-01-15 18:32:23 +0000

    [backend] Setup initial project structure - 2025-01-15

---
+++ src/main/java/pt/com/estetica/hm/HmApplication.java
@@ -0,0 +1,13 @@
+package pt.com.estetica.hm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmApplication.class, args);
+	}
+
+}
